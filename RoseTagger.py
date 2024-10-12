903c1441efb27eb39709a6699643e5ace47d649e
Cyres29 <151050053+Cyres29@users.noreply.github.com>
2024-10-12 19:14:34 +0300

    Update RoseTagger.py

--- RoseTagger.py
+++ RoseTagger.py
@@ -58,7 +58,7 @@ BOT_TOKEN = os.environ.get("TOKEN", "7705676636:AAFauY1-CPbtvHwqHbuVZ-FnLTh4MjNe
 
 BOT_ID = int(os.environ.get("BOT_ID", "8074070738"))  
 
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Cheftagbot")  
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@BuseTagger_bot")  
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002422761114))  
 OWNER_ID = 7461419909  # Sahip hesabın id'si
 
